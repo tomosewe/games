fd0e572c87615713bec23ecf27f1fe879571b822
tomosewe <tomosewe@gmail.com>
2019-06-12 21:05:50 +1200

    game winnable

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Nbsp from "./Nbsp";
@@ -24,14 +24,16 @@ const Game = () => {
   ]);
 
   const checkGameWon = () => {
-    console.log(secretWord);
     if ([...secretWord].every(letter => enteredLetters.includes(letter))) {
-      console.log("game is won");
       setGameWon(true);
     }
   };
 
-  const processLetter = async char => {
+  useEffect(() => {
+    checkGameWon();
+  });
+
+  const processLetter = char => {
     if (!enteredLetters.includes(char)) {
       setEnteredLetters([...enteredLetters, char]);
       if (!secretWord.includes(char)) {
@@ -40,15 +42,14 @@ const Game = () => {
     }
   };
 
-  const onButtonClick = async char => {
-    await processLetter(char);
-    checkGameWon();
+  const onButtonClick = char => {
+    processLetter(char);
   };
 
   const renderWord = () => (
     <section className="word">
       <Paper className="word__container">
-        {numberOfGuesses > 0 || !gameWon ? (
+        {!gameWon && numberOfGuesses > 0 ? (
           <React.Fragment>
             {[...secretWord].map((letter, index) => (
               <span
@@ -73,7 +74,7 @@ const Game = () => {
   );
 
   const renderKeyboard = () =>
-    numberOfGuesses > 0 || !gameWon ? (
+    !gameWon && numberOfGuesses > 0 ? (
       <section className="keyboard">
         {alphabet.map(char => (
           <span className="keyboard__letter" key={`keyboard-${char}`}>
@@ -113,6 +114,4 @@ export default Game;
    draw the hangman dude
    correctly wrap the letters onto new lines
    using keyboard should trigger buttons - see javascript30 first exercise
-
-
 */
